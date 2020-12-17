058de864337088c41920bed1da372404a7d4447e
baxromov <baxromov.shahzodbek@gmail.com>
2020-12-18 00:36:50 +0500

    add

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import (
+    Store,
+    Manufacturer,
+    Measure,
+    Category,
+    Product,
+)
 
-# Register your models here.
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Manufacturer)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Measure)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ['type']
+
+@admin.register(Category)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Product)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
