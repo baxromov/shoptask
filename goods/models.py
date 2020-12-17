66e19ea80fab57e7b957be007aa7d8967c1a5444
baxromov <baxromov.shahzodbek@gmail.com>
2020-12-18 00:24:51 +0500

    add

--- goods/models.py
+++ goods/models.py
@@ -4,11 +4,20 @@ from django.db import models
 class Measure(models.Model):
     type = models.CharField(max_length=50)
 
-class Store(models.Model):
-    pass
+    def __str__(self):
+        return self.type
 
-class Manufacture(models.Model):
-    pass
+class Store(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -25,6 +34,8 @@ class Product(models.Model):
     is_sale = models.BooleanField()
     sale_price = models.DecimalField(max_digits=16, decimal_places=2)
     qty = models.IntegerField()
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     img = models.ImageField(default='not_found.jpg')
 
 
